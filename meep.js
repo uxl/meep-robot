fa693e5a60bfc356ed85b502bb55d621892aae69
uxl <brian@ulx.io>
2016-02-25 19:43:18 -0500

    meep

--- meep.js
+++ meep.js
@@ -1,5 +1,12 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+
 var hydna = require('hydna');
 var channel = hydna.createChannel('http://ulx.hydna.net/test', 'rw');
+
+var board = new five.Board({
+      io: new Raspi()
+});
 
 channel.on('connect', function() {
   // read/write connection is ready to use
@@ -15,6 +22,13 @@ channel.on('error', function() {
 });
 channel.on('data', function(data) {
   console.log('Channel "%s" recieved: %s', this.url, data);
+  // Close the channel. The application will terminate once both
+  led.blink(1);
+
+});
+
+board.on("ready", function() {
+      var led = new five.Led("P1-13");
 });
 /*
 var five = require("johnny-five");
