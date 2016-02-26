71adea8c64783beaf872306a0ec0edf63365845b
uxl <brian@ulx.io>
2016-02-26 02:35:13 -0500

    added couple functions

--- meep.js
+++ meep.js
@@ -3,6 +3,7 @@ var Raspi = require("raspi-io");
 
 var hydna = require('hydna');
 var channel = hydna.createChannel('http://ulx.hydna.net/test', 'rw');
+var led = null;
 
 var board = new five.Board({
       io: new Raspi()
@@ -22,15 +23,39 @@ channel.on('error', function(e) {
 });
 channel.on('data', function(data) {
   console.log('Channel "%s" recieved: %s', this.url, data);
-  // Close the channel. The application will terminate once both
-  led.blink(1);
+  //determine device
+
+  for ( property in data ) {
+    console.log( property ); // Outputs: foo, fiz or fiz, foo
+    switch (property) {
+      case "led":
+        ledController(data[property]);
+        break;
+        case "dial":
+          dialController(data[property]);
+          break;
+      default:
+    }
+  }
 
 });
 
 board.on("ready", function() {
-      var led = new five.Led("P1-13");
-      led.blink(1);
+      led = new five.Led("P1-13");
 });
+var ledController = function(state){
+  switch(state){
+    case false:
+      led.off();
+    break;
+    case true:
+      led.on();
+    break;
+  }
+}
+var dialController = function(val){
+  console.log('dial value: ' + val)
+}
 /*
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
