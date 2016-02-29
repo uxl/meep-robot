ff62b64c4cf0fc0cf02bae8fac056b65eb87b733
uxl <brian@ulx.io>
2016-02-29 14:35:04 -0500

    update

--- meep.js
+++ meep.js
@@ -1,88 +1,108 @@
+/* global console, jQuery, $, TrackGA */
+// meep robot
+// Mullen - Wilkinson 2016
+
+'use strict';
+
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
-
 var hydna = require('hydna');
-var channel = null;
-var led = null;
-var starttime = null;
 
-var board = new five.Board({
-  io: new Raspi()
-});
+var MEEP = (function($) {
+  //vars
+  var channel = null,
+    led = null,
+    starttime = null,
+    board = null,
 
-var connectHy = function(){
-  channel = hydna.createChannel('http://ulx.hydna.net/test', 'rw');
-  channel.on('connect', function() {
-    console.log(new Date().getTime() - startTime);
-    // read/write connection is ready to use
-    console.log('connected!!');
-    var message = 'meep robot connected';
-    this.write(message);
-  });
+    init = function() {
+      console.log(MEEP.init);
+      board = new five.Board({
+        io: new Raspi()
+      });
+      //events
+      board.on("ready", function() {
+        led = new five.Led("P1-13");
+      })
+      connect();
+    },
+    connect = function() {
+      channel = hydna.createChannel('http://ulx.hydna.net/test', 'rw');
 
-  channel.on('error', function(err) {
-    // an error occured when connecting
-    console.log('error: ' + err);
-    console.log('reconnect attempt on error');
-  });
-  channel.on('close', function(err){
-    console.log('connection lost: ' + err);
-    console.log('reconnect attempt on close');
-    startTime = new Date().getTime();
-
-    return setTimeout(connectHy, 3000);
-  });
-  channel.on('data', function(cmdObj) {
-    // console.log('Channel "%s" recieved: %s', this.url, cmdObj);
-    //determine device
-      try {
-        var cmd = JSON.parse(cmdObj);
-        console.log(cmd);
-        if(cmd.hasOwnProperty('led')){
-          ledController(cmd['led']);
+      //add events
+      channel.on('connect', function() {
+        if (!connected) {
+          console.log(new Date().getTime() - startTime);
+          connected = true;
         }
-        if(cmd.hasOwnProperty('dial')){
-          dialController(cmd['dial']);
+        // read/write connection is ready to use
+        console.log('connected!!');
+        var message = 'meep robot connected';
+        this.write(message);
+      });
+
+      channel.on('error', function(err) {
+        // an error occured when connecting
+        console.log('error: ' + err);
+        console.log('reconnect attempt on error');
+      });
+      channel.on('close', function(err) {
+        console.log('connection lost: ' + err);
+        console.log('reconnect attempt on close');
+        startTime = new Date().getTime();
+
+        return setTimeout(connect, 3000);
+      });
+      channel.on('data', function(cmdObj) {
+        // console.log('Channel "%s" recieved: %s', this.url, cmdObj);
+        //determine device
+        try {
+          var cmd = JSON.parse(cmdObj);
+          console.log(cmd);
+          if (cmd.hasOwnProperty('led')) {
+            ledController(cmd['led']);
+          }
+          if (cmd.hasOwnProperty('dial')) {
+            dialController(cmd['dial']);
+          }
+        } catch (e) {
+          console.log(e);
         }
-      } catch (e) {
-        console.log(e);
+      });
+    },
+    parseCmd = function(cmd) {
+      console.log('cmd', cmd);
+
+      for (property in cmd) {
+        console.log('property', property);
+        switch (property) {
+          case "led":
+            console.log('led action');
+            ledController(cmd[property]);
+            break;
+          case "dial":
+            console.log('dial action');
+            dialController(cmd[property]);
+            break;
+          default:
+        }
       }
-  });
-};
-var parseCmd = function(cmd) {
-  console.log('cmd', cmd);
-
-  for (property in cmd) {
-    console.log('property', property);
-    switch (property) {
-      case "led":
-        console.log('led action');
-        ledController(cmd[property]);
-        break;
-      case "dial":
-        console.log('dial action');
-        dialController(cmd[property]);
-        break;
-      default:
-    }
-  }
-};
-
-board.on("ready", function() {
-  led = new five.Led("P1-13");
-});
-var ledController = function(state) {
-  switch (state) {
-    case false:
-      led.off();
-      break;
-    case true:
-      led.on();
-      break;
-  }
-}
-var dialController = function(val) {
-  console.log('dial value: ' + val)
-}
-//connect
-connectHy();
+    },
+    ledController = function(state) {
+      switch (state) {
+        case false:
+          led.off();
+          break;
+        case true:
+          led.on();
+          break;
+      }
+    },
+    dialController = function(val) {
+      console.log('dial value: ' + val)
+    };
+  //connect
+  return {
+    init: init
+  };
+}());
