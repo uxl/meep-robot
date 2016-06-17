1ebeae0ca7fa47dcd1ae0cc66aefcc9d1264f806
brian.wilkinson <brian@ulx.io>
2016-06-17 17:30:05 -0400

    pixelf.js

---
+++ pixelf.js
@@ -0,0 +1,16 @@
+    var pixel = require("node-pixel"),
+        firmata = require('firmata'),
+        board = new firmata.Board('/dev/ttyACM0',function(){
+
+        strip = new pixel.Strip({
+            pin: 6, // this is still supported as a shorthand
+            length: 4,
+            firmata: board,
+            controller: "FIRMATA",
+        });
+
+        strip.on("ready", function() {
+            // do stuff with the strip here.
+            strip.on();
+        });
+    });
