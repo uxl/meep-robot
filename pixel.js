a8ce3ba95bfa6463605ab280179590bdfb18b191
brian.wilkinson <brian@ulx.io>
2016-06-21 12:09:58 -0400

    rainbow

--- pixel.js
+++ pixel.js
@@ -28,10 +28,58 @@ console.log("board ready");
         strip.color("#ff0000"); // turns entire strip red using a hex colour
         console.log("set color");
         strip.show();
-        console.log("set show");
-        console.log(strip);
-         var p = strips[0].strip.pixel(2);
-         p.color("blue");
-         strip.show();
+        //console.log("set show");
+        //console.log(strip);
+        //  var p = strips[0].strip.pixel(2);
+        //  p.color("blue");
+        //  strip.show();
+        console.log("Strip ready, let's go");
+        dynamicRainbow(fps);
    });
+   strip.on("ready", function() {
+
+    });
+
+    function dynamicRainbow( delay ){
+        console.log( 'dynamicRainbow' );
+
+        var showColor;
+        var cwi = 0; // colour wheel index (current position on colour wheel)
+        var foo = setInterval(function(){
+            if (++cwi > 255) {
+                cwi = 0;
+            }
+
+            for(var i = 0; i < strip.stripLength(); i++) {
+                showColor = colorWheel( ( cwi+i ) & 255 );
+                strip.pixel( i ).color( showColor );
+            }
+            strip.show();
+        }, 1000/delay);
+    }
+
+    // Input a value 0 to 255 to get a color value.
+    // The colors are a transition r - g - b - back to r.
+    function colorWheel( WheelPos ){
+        var r,g,b;
+        WheelPos = 255 - WheelPos;
+
+        if ( WheelPos < 85 ) {
+            r = 255 - WheelPos * 3;
+            g = 0;
+            b = WheelPos * 3;
+        } else if (WheelPos < 170) {
+            WheelPos -= 85;
+            r = 0;
+            g = WheelPos * 3;
+            b = 255 - WheelPos * 3;
+        } else {
+            WheelPos -= 170;
+            r = WheelPos * 3;
+            g = 255 - WheelPos * 3;
+            b = 0;
+        }
+        // returns a string with the rgb value to be used as the parameter
+        return "rgb(" + r +"," + g + "," + b + ")";
+    }
 });
