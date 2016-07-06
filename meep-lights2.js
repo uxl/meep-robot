872c57257a602893bae143e885d27c8afc240595
brian.wilkinson <brian@ulx.io>
2016-07-05 21:50:40 -0400

    eve

--- meep-lights2.js
+++ meep-lights2.js
@@ -21,9 +21,9 @@ var MEEP = (function($) {
 
     stripArr = [status, dial, bar],
 
-    status = [0],
-    dial = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    bar = [13, 14, 15, 16, 17, 18, 19, 20, 21],
+    status = [0,1],
+    dial = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    bar = [14, 15, 16, 17, 18, 19, 20, 21, 22],
 
     colors = [],
     renderInt = null,
@@ -47,7 +47,7 @@ var MEEP = (function($) {
           controller: "FIRMATA",
           strips: [{
               pin: 6,
-              length: 1
+              length: 2
             }, // status
             {
               pin: 5,
