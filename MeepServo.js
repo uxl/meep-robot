0b090d434fa9b9d12f3b67686b03032593cb4498
brian.wilkinson <brian@ulx.io>
2016-07-20 16:00:52 -0400

    comment out this

--- MeepServo.js
+++ MeepServo.js
@@ -7,13 +7,13 @@ MeepServo.prototype.servo = null;
 
 MeepServo.prototype.init = function(pin, min, max) {
   console.log('pin: ' + pin + ' | min: ' + min + ' | max: ' + max);
-  this.servo = new five.Servo({
-    pin: pin,
-    range: [min, max]
-  });
+  // this.servo = new five.Servo({
+  //   pin: pin,
+  //   range: [min, max]
+  // });
 };
 MeepServo.prototype.to = function(val) {
-  this.servo.to(val);
+  //this.servo.to(val);
 };
 
 module.exports = new MeepServo();
