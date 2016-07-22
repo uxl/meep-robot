214049b728afbc8e80b255d001a945148d8e24c4
brian.wilkinson <brian@ulx.io>
2016-07-21 20:31:06 -0400

    woo

--- MeepServo.js
+++ MeepServo.js
@@ -1,13 +1,11 @@
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
 
-var servo = new five.Servo(3);
-
 var MeepServo = function() {};
 // MeepServo.prototype.list = [];
 MeepServo.prototype.init = function(pin, min, max) {
   console.log('pin: ' + pin + ' | min: ' + min + ' | max: ' + max);
-  var that = this;
+  var that.servo = this;
   that = new five.Servo({
     pin: pin,
     range: [min, max]
@@ -22,6 +20,6 @@ MeepServo.prototype.to = function(val) {
   that.servo.to(val);
 };
 MeepServo.prototype.sweep = function() {
-  servo.sweep();
+  this.servo.sweep();
 };
 module.exports = MeepServo;
